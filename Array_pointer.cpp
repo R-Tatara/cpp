642b63334b0d0351f2c86e81016ed9c6fb1585b4
R-Tatara <rabumel.dpl@gmail.com>
2018-06-13 01:52:04 +0930

    add array pointer file

---
+++ Array_pointer.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int func(float val, float array[3]);
+
+int main()
+{
+	float array[3];
+	func(1.5F, array);
+	cout << array[0] << "\t" << array[1] << "\t" << array[2] << endl;
+
+	getchar();
+	return 0;
+}
+
+int func(float val, float array[3]) {
+	array[0] = val + 1.0F;
+	array[1] = array[0] + 1.0F;
+	array[2] = array[1] + 1.0F;
+	
+	return 0;
+}
+
+

7632d4419e2f9477fd331ed880d30979e8a09b06
Tatara <rabumel.dpl@gmail.com>
2019-08-14 17:07:12 +0900

    Update

--- Class_func_template/Class_func_template/Source.cpp
+++ Class_func_template/Class_func_template/Source.cpp
@@ -1,43 +1,23 @@
 #include <iostream>
 using namespace std;
 
-void func1(float &, float &);
-void func2(float &, float &);
-
 class MyClass {
 public:
-	float a, b, c[2];
+	float a, b[2];
 	MyClass();
 };
 
 MyClass::MyClass()
 	: a(1.0),
-	b(2.0),
-	c{ 3.0, 4.0 }
+	b{ 2.0, 3.0 }
 {
 }
 
 MyClass c;
 
 int main() {
-	cout << c.a << "\t" << c.b << endl;
-	cout << c.c[0] << "\t" << c.c[1] << endl;
-	func1(c.a, c.b);
-	func2(c.c[0], c.c[1]);
-	cout << c.a << "\t" << c.b << endl;
-	cout << c.c[0] << "\t" << c.c[1] << endl;
+	cout << c.a << endl;
+	cout << c.b[0] << "\t" << c.b[1] << endl;
 	getchar();
 	return 0;
-}
-
-void func1(float &x, float &y) {
-	x = 5;
-	y = 6;
-	return;
-}
-
-void func2(float &x, float &y) {
-	x = 7;
-	y = 8;
-	return;
 }
--- file_naming/file_naming/Source.cpp
+++ file_naming/file_naming/Source.cpp
@@ -1,7 +1,5 @@
 #include <fstream>
 #include <time.h>
-#include "Source.h"
-
 
 int main() {
 	time_t tim = time(NULL);
@@ -10,15 +8,15 @@ int main() {
 	char filename[64];
 
 	error = localtime_s(&now_time, &tim);
-	
-	sprintf_s(filename, "/Users/rabum/source/repos/file_naming/%04d%02d%02d%02d%02d%02d.txt",
+
+	sprintf_s(filename, "/Users/rabum/source/repos/file_naming/%04d%02d%02d%02d%02d%02d.csv",
 		now_time.tm_year + 1900,
 		now_time.tm_mon + 1,
 		now_time.tm_mday,
 		now_time.tm_hour,
-		now_time.tm_min, 
+		now_time.tm_min,
 		now_time.tm_sec);
-	
+
 	std::ofstream ofs(filename);
 	ofs.clear();
 	ofs.seekp(0);

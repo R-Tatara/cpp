86e3fc607f247235082aa24cb9b727f15b005d17
R-Tatara <rabumel.dpl@gmail.com>
2020-02-24 01:23:24 +0900

    Update Least_squares

--- Least_squares/Least_squares/Source.cpp
+++ Least_squares/Least_squares/Source.cpp
@@ -1,13 +1,12 @@
-#include <stdio.h>
 #include <iostream>
 
 using namespace std;
 
-void LeastSquaresMethod(double x[], double y[], int N, double *a0, double *a1) {
-	int i;
+int LeastSquaresMethod(const double x[], const double y[], const int dataSize,	//Input
+					   double *a0, double *a1) {								//Output
 	double A00 = 0, A01 = 0, A02 = 0, A11 = 0, A12 = 0;
 
-	for (i = 0; i < N; i++) {
+	for (int i = 0; i < dataSize; i++) {
 		A00 += 1.0;
 		A01 += x[i];
 		A02 += y[i];
@@ -15,28 +14,35 @@ void LeastSquaresMethod(double x[], double y[], int N, double *a0, double *a1) {
 		A12 += x[i] * y[i];
 	}
 
-	*a0 = (A02 * A11 - A01 * A12) / (A00 * A11 - A01 * A01);
-	*a1 = (A00 * A12 - A01 * A02) / (A00 * A11 - A01 * A01);
+	if (A00 * A11 - A01 * A01 != 0.0) {
+		*a0 = (A02 * A11 - A01 * A12) / (A00 * A11 - A01 * A01);
+		*a1 = (A00 * A12 - A01 * A02) / (A00 * A11 - A01 * A01);
+	}
+	else {
+		cout << "[ERROR] division by zero" << endl;
+		return -1;
+	}
+
+	return 0;
 }
  
 int main() {
 	double x[] = { 1.0, 2.0, 3.0, 4.0, 5.0 };
 	double y[] = { 3.0, 4.0, 5.0, 6.0, 7.0 };
-	double a0 = 0.0; //Intercept
-	double a1 = 0.0; //Slope
-
-	//The size of array 
-	int N;
+	double intercept = 0.0;
+	double slope = 0.0;
+	int dataSize;
 	int Nx = sizeof(x) / sizeof(x[0]);
 	int Ny = sizeof(y) / sizeof(y[0]);
 
 	if (Nx == Ny) {
-		N = Nx;
-		LeastSquaresMethod(x, y, N, &a0, &a1);
-		cout << "Slope : " << a1 << "\nIntercept : " << a0 << endl;
+		dataSize = Nx;
+		LeastSquaresMethod(x, y, dataSize, &intercept, &slope);
+		cout << "Slope : " << slope << "\nIntercept : " << intercept << endl;
 	}
 	else {
 		cout << "Different size of array." << endl;
+		return -1;
 	}
 
 	getchar();

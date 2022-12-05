bab287d518b216c3c782857139e7776e35ffd0cd
R-Tatara <rabumel.dpl@gmail.com>
2022-12-05 09:33:20 +0900

    Use GetLocalTime instead of localtime_s

--- OutputTextFile/OutputTextFile/Source.cpp
+++ OutputTextFile/OutputTextFile/Source.cpp
@@ -1,6 +1,6 @@
 #include <fstream>
 #include <iostream>
-#include <time.h>
+#include <windows.h>
 #include "Header.h"
 
 //Global object definition
@@ -8,26 +8,35 @@ file logfile;
 
 //Function definition
 file InitFileName() {
-    time_t tim = time(NULL);
-    struct tm now_time;
-    errno_t error = localtime_s(&now_time, &tim);
+    SYSTEMTIME st;
 
-    sprintf_s(logfile.name, STR_SIZE, "Log_%04d%02d%02d-%02d%02d%02d.csv",
-        now_time.tm_year + 1900,
-        now_time.tm_mon + 1,
-        now_time.tm_mday,
-        now_time.tm_hour,
-        now_time.tm_min,
-        now_time.tm_sec);
+    GetLocalTime(&st);
+    sprintf_s(logfile.name, STR_SIZE, "Log_%04d%02d%02d-%02d%02d%02d_%03d.txt",
+            st.wYear, st.wMonth, st.wDay,
+            st.wHour, st.wMinute, st.wSecond,
+            st.wMilliseconds);
 
     return logfile;
-};
+}
+
+//file InitFileName() {
+//    time_t tim = time(NULL);
+//    struct tm now_time;
+//    errno_t error = localtime_s(&now_time, &tim);
+//
+//    sprintf_s(logfile.name, STR_SIZE, "Log_%04d%02d%02d-%02d%02d%02d.csv",
+//        now_time.tm_year + 1900,
+//        now_time.tm_mon + 1,
+//        now_time.tm_mday,
+//        now_time.tm_hour,
+//        now_time.tm_min,
+//        now_time.tm_sec);
+//
+//    return logfile;
+//};
 
 void AppendString(file file, const char append_str[]) {
     std::ofstream ofs(file.name, std::ios::app);
-    time_t tim = time(NULL);
-    struct tm now_time;
-    errno_t error = localtime_s(&now_time, &tim);
 
     if (!ofs) {
         std::cerr << "Failed to open file." << std::endl;
